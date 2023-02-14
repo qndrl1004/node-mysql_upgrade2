695e0d8d510d3ac402d9a04ed8fde2de09232773
qndrl1004 <qndrl1004@gmail.com>
2023-02-14 11:38:35 +0900

    수정

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,10 @@
 import express from "express";
 import * as authController from "../controller/auth.js";
-import { body } from "express-validator";
-import { validate } from "../middleware/validator.js";
+import * as validate from "../middleware/validator.js";
 const router = express.Router();
 
-const validateCredential = [
-  body("nickname")
-    .trim()
-    .notEmpty()
-    .isLength({ min: 3 })
-    .withMessage("nickname should be at least 3 characters"),
-  body("password")
-    .trim()
-    .isLength({ min: 4 })
-    .withMessage("password should be at least 4 characters"),
-  validate,
-];
-
 //회원가입
-router.post("/signup", validateCredential, authController.signup);
+router.post("/signup", validate.validate, authController.signup);
 //로그인
 router.post("/login", authController.login);
 
